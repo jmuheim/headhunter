a20409d45c27ad137fedfbb459717d41de7da5e8
Joshua Muheim <joshua.muheim@sientia.ch>
2014-01-19 13:34:50 +0100

    Undo invalid rebase stuff

--- lib/headhunter.rb
+++ lib/headhunter.rb
@@ -2,55 +2,90 @@ require 'headhunter/rails'
 require 'html_validation'
 require 'rack/utils'
 
-class Result
-  attr_accessor :url, :html, :results
-
-  def initialize(url, html, results)
-    @url     = url
-    @html    = html
-    @results = results
-  end
-end
-
 class Headhunter
   attr_accessor :results
 
+  class HtmlHunter
+    def initialize
+      @valid_results   = []
+      @invalid_results = []
+    end
+
+    def process!(url, html)
+      html_validation = PageValidations::HTMLValidation.new.validation(html, random_name)
+      (html_validation.valid? ? @valid_results : @invalid_results) << html_validation
+    end
+
+    def prepare_results_html
+      html = File.read File.dirname(File.expand_path(__FILE__)) + '/headhunter/templates/results.html'
+      html.gsub! '{{VALID_RESULTS}}', prepare_results_for(@valid_results)
+      html.gsub! '{{INVALID_RESULTS}}', prepare_results_for(@invalid_results)
+      File.open('.validation/results.html', 'w') { |file| file.write(html) }
+    end
+
+    def prepare_results_for(results)
+      results.map do |result|
+        exceptions_html = ::Rack::Utils.escape_html(File.read(".validation/#{result.resource}.exceptions.txt"))
+
+        full_result_html = File.read File.dirname(File.expand_path(__FILE__)) + '/headhunter/templates/result.html'
+        full_result_html.gsub! '{{RESOURCE}}', result.resource
+        full_result_html.gsub! '{{EXCEPTIONS}}', exceptions_html
+        full_result_html.gsub! '{{HTML_CONTEXT}}', 'context'
+        full_result_html.gsub! '{{LINK}}', "#{result.resource}.html.txt"
+
+        full_result_html
+      end.join
+    end
+
+    def report
+      log.puts "Validated #{@valid_results.size + @invalid_results.size} HTML pages.".yellow
+      log.puts "#{x_pages_be(@valid_results.size)} valid.".green
+      log.puts "#{x_pages_be(@invalid_results.size)} invalid.".red
+      log.puts 'Open .validation/results.html to view full results.'
+    end
+
+    private
+
+    def x_pages_be(size)
+      if size <= 1
+        "#{size} page is"
+      else
+        "#{size} pages are"
+      end
+    end
+
+    def random_name
+      (0...8).map { (65 + rand(26)).chr }.join
+    end
+  end
+
+  class CssHunter
+    def initialize
+    end
+
+    def process!(url, html)
+      
+    end
+
+    def report
+      
+    end
+  end
+
   def initialize
-    @valid_results   = []
-    @invalid_results = []
-    # yield self and run if block_given?
+    @html_hunter = HtmlHunter.new
+    @css_hunter  = CssHunter.new
   end
 
   def process!(url, html)
-    if is_valid_asset?(html)
-      @valid_results << Result.new(url, html, 'alles gut!')
-    else
-      @invalid_results << Result.new(url, html, 'nichts gut!')
-    end
-  end
-
-  def is_valid_asset?(html)
-    true
+    @html_hunter.process!(url, html)
+    @css_hunter.process!(url, html)
   end
 
   def report
-    log.puts "Validated #{@valid_results.size + @invalid_results.size} HTML pages.".yellow
-    log.puts "#{x_pages_be(@valid_results.size)} valid.".green
-    log.puts "#{x_pages_be(@invalid_results.size)} invalid.".red
-    log.puts 'Open .validation/results.html to view full results.'
-  end
+    @html_hunter.prepare_results_html
 
-  private
-
-  def x_pages_be(size)
-    if size <= 1
-      "#{size} page is"
-    else
-      "#{size} pages are"
-    end
-  end
-
-  def random_name
-    (0...8).map { (65 + rand(26)).chr }.join
+    @html_hunter.report
+    @css_hunter.report
   end
 end
