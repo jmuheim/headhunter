912ad78c2da205d9a9dc73b6917999d66d4ff93a
Joshua Muheim <joshua.muheim@sientia.ch>
2014-01-19 20:09:02 +0100

    Add files to gemset

--- lib/headhunter.rb
+++ lib/headhunter.rb
@@ -2,7 +2,7 @@ require 'headhunter/css_hunter'
 require 'headhunter/css_validator'
 require 'headhunter/html_validator'
 require 'headhunter/rails'
-require 'rack/utils'
+# require 'rack/utils'
 
 class Headhunter
   attr_accessor :results
