74995c8249df1cca9c9bf28c7f7223295cd7a2cf
Joshua Muheim <joshua.muheim@sientia.ch>
2014-02-05 20:40:03 +0100

    More specs

--- spec/headhunter/local_response_spec.rb
+++ spec/headhunter/local_response_spec.rb
@@ -23,6 +23,39 @@ describe Headhunter::LocalResponse do
     end
   end
 
+  describe '#errors' do
+    context 'valid response' do
+      subject { Headhunter::LocalResponse.new(read_file('valid_response.xml')) }
+
+      it 'returns an empty array' do
+        expect(subject.errors).to eq []
+      end
+    end
+
+    context 'invalid response' do
+      subject { Headhunter::LocalResponse.new(read_file('invalid_response.xml')) }
+
+      it 'returns an array of error hashes' do
+        expect(subject.errors).to eq [{line: 1,
+                                       errortype: 'parse-error',
+                                       context: 'invalid_property',
+                                       errorsubtype: 'exp',
+                                       skippedstring: '123',
+                                       message: "Property bla doesn't exist"
+                                     }]
+      end
+    end
+  end
+
+  describe '#convert_soap_to_xml' do
+    subject { Headhunter::LocalResponse.new }
+
+    it 'converts SOAP to XML' do
+      string = "this is the first line\n<m:errors><m:pipapo><bla>Bla!</bla</m:pipapo</m:errors>"
+      expect(subject.send :convert_soap_to_xml, string).to eq "<errors><pipapo><bla>Bla!</bla</pipapo</errors>"
+    end
+  end
+
   describe '#remove_first_line_from' do
     subject { Headhunter::LocalResponse.new }
 
@@ -38,15 +71,6 @@ describe Headhunter::LocalResponse do
     it 'replaces tags like <m:error> with <error> in a passed string' do
       string = "<m:errors><m:pipapo><bla>Bla!</bla</m:pipapo</m:errors>"
       expect(subject.send :sanitize_prefixed_tags_from, string).to eq "<errors><pipapo><bla>Bla!</bla</pipapo</errors>"
-    end
-  end
-
-  describe '#convert_soap_to_xml' do
-    subject { Headhunter::LocalResponse.new }
-
-    it 'converts SOAP to XML' do
-      string = "this is the first line\n<m:errors><m:pipapo><bla>Bla!</bla</m:pipapo</m:errors>"
-      expect(subject.send :convert_soap_to_xml, string).to eq "<errors><pipapo><bla>Bla!</bla</pipapo</errors>"
     end
   end
 end
