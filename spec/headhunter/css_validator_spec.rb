2d8d7a2a200254305e37d7a88b5260b05e00375e
Joshua Muheim <joshua.muheim@sientia.ch>
2014-02-07 22:50:44 +0100

    Specs for #extract_filename

--- spec/headhunter/css_validator_spec.rb
+++ spec/headhunter/css_validator_spec.rb
@@ -10,4 +10,23 @@ describe Headhunter::CssValidator do
 
     it 'throws an exception when calling the validator fails'
   end
+
+  describe '#extract_filename' do
+    subject { described_class.new }
+
+    it "extracts the file's name from a Rails' asset path" do
+      filename = '/rails/root/public/assets/application-123abc.css'
+      expect(subject.send(:extract_filename, filename)).to eq 'application.css'
+    end
+
+    it 'allows underscores in the file name' do
+      filename = '/rails/root/public/assets/some_file-123abc.css'
+      expect(subject.send(:extract_filename, filename)).to eq 'some_file.css'
+    end
+
+    it 'allows hyphens in the file name' do
+      filename = '/rails/root/public/assets/some-file-123abc.css'
+      expect(subject.send(:extract_filename, filename)).to eq 'some-file.css'
+    end
+  end
 end
