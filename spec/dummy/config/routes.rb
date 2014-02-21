f61280ddaa4f91d17bd8263fa372ffb6e71ac149
Joshua Muheim <joshua.muheim@sientia.ch>
2014-02-21 14:16:30 +0100

    Remove unneeded mount

--- spec/dummy/config/routes.rb
+++ spec/dummy/config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  mount Headhunter::Engine => "/headhunter"
-
   resources :posts, only: :index
 
   # The priority is based upon order of creation: first created -> highest priority.
