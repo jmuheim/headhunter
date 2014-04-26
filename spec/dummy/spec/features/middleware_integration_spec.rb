0748dc2167ac86431d94a4642136938fd047886a
Joshua Muheim <joshua.muheim@sientia.ch>
2014-04-26 12:24:52 +0200

    Add links to possibly related stuff

--- spec/dummy/spec/features/middleware_integration_spec.rb
+++ spec/dummy/spec/features/middleware_integration_spec.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 feature 'Middleware integration' do
   scenario "Integrating the middleware into the Rack stack" do
     pending "The expectation doesn't work, see http://stackoverflow.com/questions/21940082"
+    # More (possibly) related infos here: http://shift.mirego.com/post/68808986788/how-to-write-tests-for-rack-middleware and http://www.sinatrarb.com/testing.html 
     Headhunter::Rack::CapturingMiddleware.any_instance.should_receive(:call)
     visit posts_path
   end
