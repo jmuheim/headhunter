17fdd74879a323f6e1c763fa8b9b7836e10eb3e0
Thomas Kriechbaumer <kriechbaumer@gmail.com>
2014-07-21 11:10:58 +0200

    use new RSpec expect-syntax

--- spec/dummy/spec/features/middleware_integration_spec.rb
+++ spec/dummy/spec/features/middleware_integration_spec.rb
@@ -3,8 +3,8 @@ require 'spec_helper'
 feature 'Middleware integration' do
   scenario "Integrating the middleware into the Rack stack" do
     pending "The expectation doesn't work, see http://stackoverflow.com/questions/21940082"
-    # More (possibly) related infos here: http://shift.mirego.com/post/68808986788/how-to-write-tests-for-rack-middleware and http://www.sinatrarb.com/testing.html 
-    Headhunter::Rack::CapturingMiddleware.any_instance.should_receive(:call)
+    # More (possibly) related infos here: http://shift.mirego.com/post/68808986788/how-to-write-tests-for-rack-middleware and http://www.sinatrarb.com/testing.html
+    expect_any_instance_of(Headhunter::Rack::CapturingMiddleware).to receive(:call)
     visit posts_path
   end
 end
